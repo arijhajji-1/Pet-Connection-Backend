a9cafe157dc5575c9f9e077facca8d255f8eae04
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-03-09 22:42:42 +0100

    f

--- controller/UserController.js
+++ controller/UserController.js
@@ -89,7 +89,7 @@ const getAll = async (req, res, next) => {
         console.log(err)
     } //kk
 }
-co  
+ 
 const profile = async (req, res) => {
     try { 
         await User.findById(req.params.id).then(result => {
