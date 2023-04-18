57d4dc516ce22586e48485e21c53e67ca2595af9
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-04-18 02:07:40 +0200

    small

--- routes/Association/association.js
+++ routes/Association/association.js
@@ -13,7 +13,7 @@ const path = require("path");
 
 const { upload } = require("../../controller/UserController");
 
-const { addAssociation, getAllAssociations, deleteAssociation, getOneAssociation, getAssociationByUser, editAssociation, upgradeToAssociation} = require("../../controller/AssociationController");
+const { addAssociation, getAllAssociations, deleteAssociation, deleteAllAssociations, getOneAssociation, getAssociationByUser, editAssociation, upgradeToAssociation} = require("../../controller/AssociationController");
 
 router.post("/addAssociation",  upload.single('file'), addAssociation);
 router.get("/allAssociations",   getAllAssociations);
@@ -21,7 +21,9 @@ router.delete("/delete/:id", deleteAssociation);
 router.get("/getOneAssociation/:id", getOneAssociation);
 router.get("/getAssociationByUser/:id", getAssociationByUser);
 router.put("/editAssociation/:id", upload.single('file'), editAssociation);
-router.post("/upgradeToAssociation",  upgradeToAssociation);
+router.post("/upgradeToAssociation", upgradeToAssociation);
+router.delete("/deleteAllAssociations", deleteAllAssociations);
+
 
 
 
